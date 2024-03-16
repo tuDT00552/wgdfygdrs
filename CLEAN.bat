5271285ff3ff192c3e5a4a6691adc9ce39f4e327
TuDT <tudt00552@fpt.edu.vn>
2024-03-17 00:01:02 +0700

    up

--- CLEAN.bat
+++ CLEAN.bat
@@ -1,3 +1,4 @@
 @echo off
-PowerShell.exe -Command "Start-Process PowerShell.exe -ArgumentList '-ExecutionPolicy Bypass -File \"%~dp0delete_program_files.ps1\"' -Verb RunAs"
+cd /d "%~dp0\wgdfygdrs"
+PowerShell.exe -ExecutionPolicy Bypass -File "delete_program_files.ps1"
 exit
